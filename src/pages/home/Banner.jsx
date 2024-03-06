13df3075864885a05c7d258ed6cac4ca43cbac66
jillurcf <jrtushar@gmail.comm>
2024-03-06 14:42:05 -0800

    responsive done for tablet and mobile

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,76 +1,87 @@
 import { FaSearch } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 const Banner = () => {
   return (
     <div className="">
       <div className="relative">
         <div className="max-w-screen-xl mx-auto">
-          <h1 className="pt-24 absolute items-center text-6xl text-blue-500 font-bold">
+          <h1 className="lg:pt-24 pt-4 pl-4 absolute items-center lg:text-6xl text-blue-500 font-bold">
             <span className=" text-white">Welcome To</span> AirBook
           </h1>
-          <div className="absolute h-[300px] w-[1280px] bg-white mt-44 rounded-xl">
-           <div className="flex gap-2 pt-16 pb-4 pl-12">
-           <div className="max-w-32 p-2 bg-gray-200 rounded-md flex gap-2">
+          <div className="absolute lg:h-[300px] lg:w-[1280px] lg:bg-white bg-none lg:mt-44 rounded-xl">
+            <div className="flex justify-between">
+            <div className="flex gap-2 pt-16 pb-4 lg:pl-12 pl-4">
+              <div className="max-w-32 p-2 bg-gray-200 rounded-md flex gap-2">
                 <div>
-                    <input type="radio" />
+                  <input type="radio" />
                 </div>
                 <h6>One Way</h6>
-            </div>
-           <div className="max-w-32 p-2 bg-blue-400 rounded-md flex gap-2">
+              </div>
+              <div className="max-w-32 p-2 bg-blue-400 rounded-md flex gap-2">
                 <div>
-                    <input type="radio" />
+                  <input type="radio" />
                 </div>
                 <h6>Round Trip</h6>
+              </div>
             </div>
-           </div>
-           <div className="flex gap-8 pt-2 pl-12">
-           <div className="max-w-70 p-6 border rounded-lg flex gap-2">
+            <div className="lg:flex gap-2 pt-16 pb-4 lg:pl-12 pl-4 pr-24 hidden">
+              <div className="max-w-32 p-2 bg-gray-200 rounded-md flex gap-2">
+                <div>
+                 <h5 className="text-blue-400">Traveller</h5>
+                </div>
+                <IoIosArrowDown></IoIosArrowDown>
+              </div>
+              <div className="max-w-32 p-2 bg-gray-200 rounded-md flex gap-2">
+              <div>
+                 <h5 className="text-blue-400">Economy</h5>
+                </div>
+                <IoIosArrowDown></IoIosArrowDown>
+              </div>
+            </div>
+            </div>
+            <div className="grid lg:grid-cols-5 md:grid-cols-2 lg:gap-2 lg:pt-6 min-[375px]:pt-8 min-[425px]:pt-12 lg:pl-12 min-[320px]:pl-6 min-[375px]:pl-12 min-[425px]:pl-20">
+              <div className="lg:p-2 p-2 border rounded-lg flex gap-2">
                 <div className="border-r-2 border-gray-200 h-full items-center">
-                    <h5 className="pr-2 h-full items-center flex">DAC</h5>
+                  <h5 className="pr-2 h-full items-center flex">DAC</h5>
                 </div>
                 <div className="">
-                    <h3>Dhaka</h3>
-                    <p>Hazrat Shahjalal International</p>
+                  <h3>Dhaka</h3>
+                  <p>Hazrat Shahjalal International</p>
                 </div>
-               
-            </div>
-           <div className="max-w-70 p-6 border rounded-lg flex gap-2">
+              </div>
+              <div className="lg:p-2 p-2 border rounded-lg flex gap-2">
                 <div className="border-r-2 border-gray-200 h-full items-center">
-                    <h5 className="pr-2 h-full items-center flex">Cxb</h5>
+                  <h5 className="pr-2 h-full items-center flex">Cxb</h5>
                 </div>
                 <div className="">
-                    <h3>Cox Bazar</h3>
-                    <p>Cox Bazar International</p>
+                  <h3>Cox Bazar</h3>
+                  <p>Cox Bazar International</p>
                 </div>
-               
-            </div>
-           <div className="max-w-70 p-6 border rounded-lg flex gap-2">
+              </div>
+              <div className="lg:p-2 p-2 border rounded-lg flex gap-2">
                 <div className="border-r-2 border-gray-200 h-full items-center">
-                    <h5 className="pr-2 h-full items-center flex">03</h5>
+                  <h5 className="pr-2 h-full items-center flex">03</h5>
                 </div>
                 <div className="">
-                    <h3>October</h3>
-                    <p>Tuesday, 2023</p>
+                  <h3>October</h3>
+                  <p>Tuesday, 2023</p>
                 </div>
-               
-            </div>
-           <div className="max-w-70 p-6 border rounded-md flex gap-2">
+              </div>
+              <div className="p-6 border rounded-md flex gap-2">
                 <div className="border-r-2 border-gray-200 h-full items-center">
-                    <h5 className="pr-2 h-full items-center flex">05</h5>
+                  <h5 className="pr-2 h-full items-center flex">05</h5>
                 </div>
                 <div className="">
-                    <h3>October</h3>
-                    <p>Tuesday, 2023</p>
+                  <h3>October</h3>
+                  <p>Tuesday, 2023</p>
                 </div>
-               
+              </div>
+              <div className="">
+                <button className="btn btn-warning p-12">
+                  <FaSearch className="text-4xl -mt-4 "></FaSearch>
+                </button>
+              </div>
             </div>
-           <div className="max-w-70 p-6 bg-warning rounded-md flex gap-2">
-           <button className="btn btn-warning">
-                <FaSearch className="text-4xl"></FaSearch>
-            </button>
-               
-            </div>
-           
-           </div>
           </div>
         </div>
 
